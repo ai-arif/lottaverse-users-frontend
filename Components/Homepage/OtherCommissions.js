97474385fc32a3d40b55747fd19c331a6a2f21d0
Md. Mainul Hasan <55666627+Mainulhasan05@users.noreply.github.com>
2024-05-28 21:43:39 +0600

    chore: Update OtherCommissions component to display user earnings and bonuses

--- Components/Homepage/OtherCommissions.js
+++ Components/Homepage/OtherCommissions.js
@@ -1,25 +1,32 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const OtherCommissions = () => {
+  const {user}=useSelector(state=>state.user)
   return (
     <div class="row">
       <div class="col-md-12">
         <table className="table text-white table-dark">
             <thead>
             <tr>
-            <th>My Earing</th>
+            <td>My Earing</td>
+            <td>{user?.earnings}</td>
             </tr>
             <tr>
                 <td>Jackpot Fund</td>
+                <td>{user?.jackpotEarnings}</td>
             </tr>
             <tr>
                 <td>Referral Commission</td>
+                <td>{user?.commissionEarnings}</td>
             </tr>
             <tr>
                 <td>Leader bonus</td>
+                <td>{user?.leaderboardEarnings}</td>
             </tr>
             <tr>
                 <td>Founder Bonus</td>
+                <td>{user?.founder}</td>
             </tr>
             </thead>
 
