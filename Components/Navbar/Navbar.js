e1ff6fdd25d795c4150087d407941bd0ffcc0141
Md. Mainul Hasan <55666627+Mainulhasan05@users.noreply.github.com>
2024-06-02 14:36:50 +0600

    add

--- Components/Navbar/Navbar.js
+++ Components/Navbar/Navbar.js
@@ -7,6 +7,7 @@ import Cookies from 'js-cookie';
 import { useRouter } from 'next/router';
 const Navbar = () => {
   const {user}=useSelector(state=>state.user)
+  
   const dispatch = useDispatch()
   const router = useRouter()
 
@@ -59,152 +60,31 @@ const Navbar = () => {
         <nav className="header-nav ms-auto">
           <ul className="d-flex align-items-center">
 
-            <li className="nav-item d-block d-lg-none">
+            {/* <li className="nav-item d-block d-lg-none">
               <a className="nav-link nav-icon search-bar-toggle " href="#">
                 <i className="bi bi-search"></i>
               </a>
-            </li>
+            </li> */}
 
             <li className="nav-item dropdown">
 
               <a className="nav-link nav-icon" href="#" data-bs-toggle="dropdown">
-                <i className="bi bi-bell"></i>
-                <span className="badge bg-primary badge-number">4</span>
+              <div class="btn-group">
+  <button type="button" class="btn btn-danger dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+    {user?.address}
+  </button>
+  <ul class="dropdown-menu">
+    <li onClick={handleSignout}><a class="dropdown-item" href="#">Logout</a></li>
+    
+    
+    
+    
+  </ul>
+</div>
               </a>
-
-              <ul className="dropdown-menu dropdown-menu-end dropdown-menu-arrow notifications">
-                <li className="dropdown-header">
-                  You have 4 new notifications
-                  <a href="#"><span className="badge rounded-pill bg-primary p-2 ms-2">View all</span></a>
-                </li>
-                <li>
-                  <hr className="dropdown-divider" />
-                </li>
-
-                <li className="notification-item">
-                  <i className="bi bi-exclamation-circle text-warning"></i>
-                  <div>
-                    <h4>Lorem Ipsum</h4>
-                    <p>Quae dolorem earum veritatis oditseno</p>
-                    <p>30 min. ago</p>
-                  </div>
-                </li>
-
-                <li>
-                  <hr className="dropdown-divider" />
-                </li>
-
-                <li className="notification-item">
-                  <i className="bi bi-x-circle text-danger"></i>
-                  <div>
-                    <h4>Atque rerum nesciunt</h4>
-                    <p>Quae dolorem earum veritatis oditseno</p>
-                    <p>1 hr. ago</p>
-                  </div>
-                </li>
-
-                <li>
-                  <hr className="dropdown-divider" />
-                </li>
-
-                <li className="notification-item">
-                  <i className="bi bi-check-circle text-success"></i>
-                  <div>
-                    <h4>Sit rerum fuga</h4>
-                    <p>Quae dolorem earum veritatis oditseno</p>
-                    <p>2 hrs. ago</p>
-                  </div>
-                </li>
-
-                <li>
-                  <hr className="dropdown-divider" />
-                </li>
-
-                <li className="notification-item">
-                  <i className="bi bi-info-circle text-primary"></i>
-                  <div>
-                    <h4>Dicta reprehenderit</h4>
-                    <p>Quae dolorem earum veritatis oditseno</p>
-                    <p>4 hrs. ago</p>
-                  </div>
-                </li>
-
-                <li>
-                  <hr className="dropdown-divider" />
-                </li>
-                <li className="dropdown-footer">
-                  <a href="#">Show all notifications</a>
-                </li>
-
-              </ul>
 
             </li>
 
-            <li className="nav-item dropdown">
-
-              <a className="nav-link nav-icon" href="#" data-bs-toggle="dropdown">
-                <i className="bi bi-chat-left-text"></i>
-                <span className="badge bg-success badge-number">3</span>
-              </a>
-
-              <ul className="dropdown-menu dropdown-menu-end dropdown-menu-arrow messages">
-                <li className="dropdown-header">
-                  You have 3 new messages
-                  <a href="#"><span className="badge rounded-pill bg-primary p-2 ms-2">View all</span></a>
-                </li>
-                <li>
-                  <hr className="dropdown-divider" />
-                </li>
-
-                <li className="message-item">
-                  <a href="#">
-                    <img src="assets/img/messages-1.jpg" alt="" className="rounded-circle" />
-                    <div>
-                      <h4>Maria Hudson</h4>
-                      <p>Velit asperiores et ducimus soluta repudiandae labore officia est ut...</p>
-                      <p>4 hrs. ago</p>
-                    </div>
-                  </a>
-                </li>
-                <li>
-                  <hr className="dropdown-divider" />
-                </li>
-
-                <li className="message-item">
-                  <a href="#">
-                    <img src="/assets/img/messages-2.jpg" alt="" className="rounded-circle" />
-                    <div>
-                      <h4>Anna Nelson</h4>
-                      <p>Velit asperiores et ducimus soluta repudiandae labore officia est ut...</p>
-                      <p>6 hrs. ago</p>
-                    </div>
-                  </a>
-                </li>
-                <li>
-                  <hr className="dropdown-divider" />
-                </li>
-
-                <li className="message-item">
-                  <a href="#">
-                    <img src="/assets/img/messages-3.jpg" alt="" className="rounded-circle" />
-                    <div>
-                      <h4>Address</h4>
-                      <p>Velit asperiores et ducimus soluta repudiandae labore officia est ut...</p>
-                      <p>8 hrs. ago</p>
-                    </div>
-                  </a>
-                </li>
-                <li>
-                  <hr className="dropdown-divider" />
-                </li>
-
-                <li className="dropdown-footer">
-                  <a href="#">Show all messages</a>
-                </li>
-
-              </ul>
-
-            </li>
 
 
           </ul>
