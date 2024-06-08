9482fe132a019019ab03b8bf9bf82c2d0c806d5d
Md. Mainul Hasan <55666627+Mainulhasan05@users.noreply.github.com>
2024-06-08 19:54:30 +0600

    api update

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   env:{
-    API:"http://localhost:5000",
+    API:"https://lottaverse.mainulhasan05.xyz",
     TOKEN_ADDRESS:"0xc59E7A9D6C9E17c987d872fF87FC4bC13c2E0B80",
   }
 };
