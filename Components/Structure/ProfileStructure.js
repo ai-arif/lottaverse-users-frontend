9dec1568aec28927ee70436eeaaf710813c207bf
atikur-24 <atikurrahman7223@gmail.com>
2024-06-10 14:15:37 +0600

    dashboard my team pages color changes done

--- Components/Structure/ProfileStructure.js
+++ Components/Structure/ProfileStructure.js
@@ -1,19 +1,18 @@
-import React from 'react';
+import React from "react";
 
 const ProfileStructure = ({ name }) => {
   return (
     <div>
-    <div className="d-flex flex-column align-items-center">
-      <div className="rounded-circle overflow-hidden bg-primary" style={{ width: '150px', height: '150px' }}>
-        <img src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png" alt="Profile" className="img-fluid rounded-circle" style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
-      </div>
-    </div>
-    <div className="border  border-1">
-        <div className="text-center bg-secondary p-3">
-            <h5 className='text-white'>{name}</h5>
+      <div className="d-flex flex-column align-items-center">
+        <div className="rounded-circle overflow-hidden bg-primary" style={{ width: "150px", height: "150px" }}>
+          <img src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png" alt="Profile" className="img-fluid rounded-circle" style={{ width: "100%", height: "100%", objectFit: "cover" }} />
         </div>
-        
-    </div>
+      </div>
+      <div className="border  border-1">
+        <div className="text-center p-3">
+          <h5 className="text-white">{name}</h5>
+        </div>
+      </div>
     </div>
   );
 };
