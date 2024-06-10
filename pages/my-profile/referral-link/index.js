83e3fca77bf07e8af63e5078d3a199172a3669ae
atikur-24 <atikurrahman7223@gmail.com>
2024-06-10 14:11:32 +0600

    dashboard profile page color changes done

--- pages/my-profile/referral-link/index.js
+++ pages/my-profile/referral-link/index.js
@@ -1,19 +1,18 @@
-import Head from 'next/head'
-import React from 'react'
-import toast from 'react-hot-toast'
-import { useSelector } from 'react-redux'
+import Head from "next/head";
+import React from "react";
+import toast from "react-hot-toast";
+import { useSelector } from "react-redux";
 const index = () => {
-  const {user}=useSelector(state=>state.user)
+  const { user } = useSelector((state) => state.user);
 
-  const handleCopyText = (textToCopy) => {  
-    navigator.clipboard.writeText(textToCopy)
+  const handleCopyText = (textToCopy) => {
+    navigator.clipboard
+      .writeText(textToCopy)
       .then(() => {
-        
-        toast.success('Copied to clipboard');
+        toast.success("Copied to clipboard");
       })
       .catch((error) => {
-        
-        toast.error('Failed to copy to clipboard');
+        toast.error("Failed to copy to clipboard");
         // Handle error here
       });
   };
@@ -22,10 +21,10 @@ const index = () => {
       <Head>
         <title>Referral Link</title>
       </Head>
-      <div className="container-fluid p-4 bg-dark">
+      <div className="container-fluid bg-transparent p-4">
         {/* create two input fields read only, Your Referral Id, Your Referral Link */}
         <div className="row p-4">
-  {/* <div className="col-md-6">
+          {/* <div className="col-md-6">
     <div className="form-group">
       <label htmlFor="exampleInputEmail1" className="text-white">Your Referral Id</label>
       <div className="input-group">
@@ -38,25 +37,32 @@ const index = () => {
       </div>
     </div>
   </div> */}
-  <div className="col-md-10">
-    <div className="form-group">
-      <label htmlFor="exampleInputPassword1" className="text-white">Your Referral Link</label>
-      <div className="input-group">
-        <input type="text" className="form-control" id="exampleInputPassword1" value={user?.referralLink} readOnly/>
-        <div className="input-group-append">
-          <button onClick={()=>{
-            handleCopyText(user?.referralLink)
-          
-          }} style={{background:"#4328f9","color":"white","border":"none"}} className="btn btn-outline-secondary p-2 copy-btn" type="button">Copy</button>
+          <div className="col-md-10">
+            <div className="form-group">
+              <label htmlFor="exampleInputPassword1" className="text-white">
+                Your Referral Link
+              </label>
+              <div className="input-group">
+                <input type="text" className="form-control bg-transparent text-white" id="exampleInputPassword1" value={user?.referralLink} readOnly />
+                <div className="input-group-append">
+                  <button
+                    onClick={() => {
+                      handleCopyText(user?.referralLink);
+                    }}
+                    style={{ background: "#4328f9", color: "white", border: "none" }}
+                    className="btn btn-outline-secondary p-2 copy-btn"
+                    type="button"
+                  >
+                    Copy
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-</div>
+  );
+};
 
-      </div>
-    </div>
-  )
-}
-
-export default index
+export default index;
