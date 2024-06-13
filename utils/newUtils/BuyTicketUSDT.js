b7a84f37540b62300cf0c22321a20f967e7a45da
Ayush <ayushranjanofficial27@gmail.com>
2024-06-13 11:55:32 +0530

    Added Metamask SDK in all functions.

--- utils/newUtils/BuyTicketUSDT.js
+++ utils/newUtils/BuyTicketUSDT.js
@@ -5,7 +5,12 @@ import {
   tokenAddress,
   tokenABI,
 } from "../../constants";
+import MetaMaskSDK from "@metamask/sdk";
 
+
+const MMSDK = new MetaMaskSDK({  dappMetadata: {
+  name: "LottaVerse DApp",
+},});
 //
 export async function _BuyTicketsUSDT(
     _tokenAddress,
@@ -18,16 +23,21 @@ export async function _BuyTicketsUSDT(
     _mainAccountAmountReff,
     _payWithRewardReff) {
     try {
-    // A Web3Provider wraps a standard Web3 provider, which is
-    // what MetaMask injects as window.ethereum into each page
-    const provider = new ethers.providers.Web3Provider(window.ethereum)
-
-    // MetaMask requires requesting permission to connect users accounts
-    await provider.send("eth_requestAccounts", []);
-
-    // The MetaMask plugin also allows signing transactions to
-    // send ether and pay to change state within the blockchain.
-    // For this, you need the account signer...
+      const ethereum = await MMSDK.getProvider();
+    
+      // This opens the app correctly, ask form permission, and gets back to the browser
+      await ethereum.request({ method: 'eth_requestAccounts', params: [] });
+      const provider = ((window.ethereum != null) ? new ethers.providers.Web3Provider(window.ethereum) : ethers.providers.getDefaultProvider());
+      // A Web3Provider wraps a standard Web3 provider, which is
+      // what MetaMask injects as window.ethereum into each page
+      //const provider = new ethers.providers.Web3Provider(window.ethereum)
+  
+      // MetaMask requires requesting permission to connect users accounts
+      // *** await provider.send("eth_requestAccounts", []);
+  
+      // The MetaMask plugin also allows signing transactions to
+      // send ether and pay to change state within the blockchain.
+      // For this, you need the account signer...
     
     const signer = provider.getSigner()
     const LOTTERYContract = new ethers.Contract(LOTTERY_CONTRACT_ADDRESS, LOTTERY_CONTRACT_ABI, provider);
